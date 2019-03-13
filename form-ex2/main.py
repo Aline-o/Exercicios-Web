66daf8ede21f403565cecef40e91f647ae13c8e6
Aline Oliveira <44536353+Nyuu-chan@users.noreply.github.com>
2019-03-13 11:08:48 -0300

    Add files via upload

---
+++ form-ex2/main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+bd={"usuario":"alynetetea@hotmail.com", "senha":"12345"}
+
+def usuario_existe(usuario):
+	return usuario==bd["usuario"]
+
+def verifica_senha(usuario, senha):
+	return usuario==bd["usuario"] and senha==bd["senha"]
+
+@app.route("/")
+def student():
+	return render_template("student.html")
+	
+@app.route("/login")
+def login():
+	return render_template("login.html")
+	
+@app.route("/loginresult")
+def login_result():
+	if request.method == "POST":
+		result = request.form
+	if usuario_existe(result("email")):
+	
+		if verifica_senha(result("email").result("senha")):
+			return render_template("loginresult.html")
+		else:
+			return render_template("loginresult_senha_incorreta.html")
+			
+	else:
+		return render_template("loginresult_usuario_incorreto.html")
+
+
+@app.route("/result", methods=['POST'])
+def result():
+	if request.method == "POST":
+		result = request.form
+	print("result")
+	print(result)
+	
+	
+	return render_template("result.html", result)
+
+if __name__== "__main__":
+	app.run(host="0.0.0.0",debug=True)
