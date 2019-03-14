ba29d14ecc2f8585c80438735abe9c60ebbd0423
Aline Oliveira <44536353+Nyuu-chan@users.noreply.github.com>
2019-03-14 19:50:49 -0300

    Add files via upload
    Exercício 3 funcionando corretamente.

---
+++ form-ex3(consertado)/main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+bd={"usuario":"alynetetea@hotmail.com", "senha":"12345"}
+
+def usuario_existe(usuario):
+	return usuario==bd["usuario"]
+
+def verifica_senha(usuario, senha):
+	return usuario==bd["usuario"] and senha==bd["senha"]
+
+@app.route("/")
+def student():
+	return render_template("student.html")
+	
+@app.route("/login")
+def login():
+	return render_template("login.html")
+	
+@app.route("/loginresult", methods=['POST'])
+def login_result():
+	if request.method == "POST":
+		response = request.form
+		print("response")
+		print(response)
+		
+	result={"status":"correto"}
+	if usuario_existe(response["email"]):
+	
+		if verifica_senha(response["email"],response["senha"]):
+			return render_template("loginresult.html", result=result)
+		else:
+			result["status"]="senha_incorreta"
+			return render_template("loginresult.html", result=result)
+			
+	else:
+		result["status"]="usuario_incorreto"
+		return render_template("loginresult.html", result=result)
+
+
+@app.route("/result", methods=['POST'])
+def result():
+	if request.method == "POST":
+		result = request.form
+	print("result")
+	print(result)
+	
+	
+	return render_template("result.html")
+
+if __name__== "__main__":
+	app.run(host="0.0.0.0",debug=True)
